da58d42b2111268e64cdbba960bb85c7b8eb009f
Alessandro Corbetta <corbisoft@gmail.com>
2019-12-27 12:34:22 +0100

    update

--- gui_TNT.py
+++ gui_TNT.py
@@ -2,14 +2,18 @@ import pandas as pd
 import PySimpleGUI as sg
 
 df = pd.read_csv("./dump_release_tntvillage_2019-08-30.csv")
+HOW_MANY_ITEMS_INIT=50
 
 def table_example():
     sg.set_options(auto_size_buttons=True)
     data = df.iloc[:50].values.tolist()
     #print (data)
+    how_many_items_to_show = HOW_MANY_ITEMS_INIT
     layout = [
-        [sg.Input(size=(20, 1), enable_events=True, key='-INPUT-')],        
-        [sg.Button('Chrome'), sg.Button('Exit')],
+        [sg.Text('Ricerca:'), sg.Input(size=(20, 1), enable_events=True, key='-INPUT-')],        
+        [sg.Input('How many',key='-HOW-MANY-')
+         , sg.Button('Chrome'), sg.Button('Exit')],
+        [sg.Text('Magnet', key='-TEXT-MAG-')],
         [sg.Table(values=data,
                   headings=list(df.columns),
                   key='-TABLE-',
@@ -17,15 +21,17 @@ def table_example():
                   auto_size_columns=False,
                   num_rows=min(25, len(data)))]
     ]
-
+    ## magnet:?xt=urn:btih:dasda
     window = sg.Window('Table', layout, grab_anywhere=False)
     while True:
         event, values = window.read()
         if event in (None, 'Exit'):                # always check for closed window
             break
         if values['-INPUT-'] != '':                         # if a keystroke entered in search field
-            search = df[df.TITOLO.str.contains(values['-INPUT-'])] #values['-INPUT-']
-            window['-TABLE-'].update(search.iloc[:50].values.tolist())
+            search = df[df.TITOLO.str.contains(values['-INPUT-']) |
+                        df.DESCRIZIONE.str.contains(values['-INPUT-'])
+            ] #values['-INPUT-']
+            window['-TABLE-'].update(search.iloc[:how_many_items_to_show].values.tolist())
             #new_values = [x for x in names if search in x]  # do the filtering
             #window['-LIST-'].update(new_values)     # display in the listbox
         else:
